431cc5acfd3a1223e9f5c62c41bd344f87cf0351
ragnarisxx <ragnarisxx@gmail.com>
2015-08-19 15:30:25 +0000

    changed key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,9 +67,9 @@ Hadean::Application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['ktib'],
-    :access_key_id => ENV['AKIAIH5CAZJ2RGIKPV4Q'],
-    :secret_access_key => ENV['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']
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
 
